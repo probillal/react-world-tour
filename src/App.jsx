8778dbe49e258ad3aea1ee8f70c43115abf9af1c
probillal <smbillalhossain273@gmail.com>
2026-03-17 22:42:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Countries from "./Components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>In the countries</h1>
+          <Countries></Countries>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
