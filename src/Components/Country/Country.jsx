95639d6ff467abdccb8d495cbc66d596329f3753
probillal <smbillalhossain273@gmail.com>
2026-03-18 11:11:43 +0600

    added conditionaly css class

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./country.css";
 const Country = ({ country }) => {
-  console.log(country.area.area);
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    //    Basic system
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    // 2nd system
+    // setVisited(visited ? false : true);
+
+    // 3rd system
+    setVisited(!visited);
+  };
+  //   console.log(country.area.area);
   return (
-    <div className="country">
+    <div className={`country ${visited && "bg-lime-500 text-blue-600"}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h2>Name: {country.name.common}</h2>
       <p>Population: {country.population.population}</p>
       <p>Capital: {country.capital.capital} </p>
       <p>Official Name: {country.name.official} </p>
       <h3 className="text-[#9980ff]">
-        Area:{country.area.area}{" "}
+        Area: {country.area.area}{" "}
         {country.area.area > 300000 ? "Big country" : "Small country"}
       </h3>
-      <button className="btn btn-primary mt-5">Not Visited</button>
+      <button onClick={handleVisited} className="btn btn-primary mt-5">
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
